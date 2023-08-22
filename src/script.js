82544ff3e5d7778d920a85fe9d72f62a7a608f37
Teagar <cyanofc@gmail.com>
2023-08-21 22:17:03 -0300

    improved star

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
+const estrela = "estrela";
 const items = [
   "Ombros", "Cicatrizes", "Risada", "Olhos", "Amigos",
   "Braços", "Peso", "Estômago", "Peitos", "Mãos",
-  "Pescoço", "Pernas", "★", "Rosto", "Altura",
+  "Pescoço", "Pernas", estrela, "Rosto", "Altura",
   "Acadêmico", "Quadris", "Pés", "Orelha", "Cabelo",
   "Tamanho", "Voz", "Nariz", "Família", "Boca"
 ];
@@ -14,6 +15,10 @@ for (let i = 0; i < 25; i++) {
   const square = document.createElement("div");
   square.className = "square";
   square.textContent = items[i];
+  if (items[i] === "estrela") {
+    square.innerHTML = "&#9733;";
+    square.classList.add("star");
+  }
   bingoContainer.appendChild(square);
 
   let colorIndex = 0;
